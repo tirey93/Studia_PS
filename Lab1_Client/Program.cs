6d759ab23f9b780925d6fae40c774441bdb658d8
Damian Opolski <dapyt@eg.dk>
2024-11-29 18:51:29 +0100

    Add project files.

---
+++ Lab1_Client/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, Client!");
+while (true)
+{
+    Console.WriteLine("another text");
+    Thread.Sleep(1000);
+}
---
+++ Lab1_Server/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, Server!");
+while (true)
+{
+    Console.WriteLine("tekst");
+    Thread.Sleep(1000);
+}
